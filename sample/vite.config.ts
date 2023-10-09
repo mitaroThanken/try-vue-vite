126d09370546581fd4532e0fcf53867cd8e6a7c8
YAMAMI Taro <mitarothanken@gmail.com>
2023-10-09 12:01:24 +0000

    replace sample
    remove sample and
    
    ```shell
    node ➜ /workspaces/sample (feature/replace-sample-create-vue) $ npm create vue@latest
    Need to install the following packages:
      create-vue@3.7.5
    Ok to proceed? (y) y
    
    Vue.js - The Progressive JavaScript Framework
    
    ✔ Project name: … sample
    ✔ Add TypeScript? … No / Yes
    ✔ Add JSX Support? … No / Yes
    ✔ Add Vue Router for Single Page Application development? … No / Yes
    ✔ Add Pinia for state management? … No / Yes
    ✔ Add Vitest for Unit Testing? … No / Yes
    ✔ Add an End-to-End Testing Solution? › No
    ✔ Add ESLint for code quality? … No / Yes
    ✔ Add Prettier for code formatting? … No / Yes
    
    Scaffolding project in /workspaces/sample/sample...
    
    Done. Now run:
    
      cd sample
      npm install
      npm run format
      npm run dev
    ```

--- sample/vite.config.ts
+++ sample/vite.config.ts
@@ -1,51 +1,16 @@
-import { CorsOptions, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import fs from 'fs';
-import { SecureContextOptions } from 'tls';
-import legacy from '@vitejs/plugin-legacy';
+import { fileURLToPath, URL } from 'node:url'
 
-const HOST = 'sample';
-const PORT = 5173;
-const HMR_PORT = 5174;
-const VHOST_FQDN = process.env.VHOST_FQDN !== undefined ? process.env.VHOST_FQDN : "test.test";
-const HTTPS_OPTIONS: SecureContextOptions = {
-  key: fs.readFileSync(`../certs/${VHOST_FQDN}/key.pem`),
-  cert: fs.readFileSync(`../certs/${VHOST_FQDN}/cert.pem`)
-};
-const PROTOCOL = 'https://';
-const CORS_OPTION: CorsOptions = {
-  origin: `${PROTOCOL}${VHOST_FQDN}`
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    legacy({
-      targets: [
-        '>0.2%',
-        'not dead',
-        'not op_mini all',
-        'IE 11'
-      ]
-    })],
-  server: {
-    host: HOST,
-    origin: `${PROTOCOL}${VHOST_FQDN}`,
-    https: HTTPS_OPTIONS,
-    hmr: {
-      path: "/hmr",
-      port: HMR_PORT,
-      clientPort: 443
-    },
-    strictPort: true,
-    cors: CORS_OPTION
-  },
-  preview: {
-    host: HOST,
-    port: PORT,
-    https: HTTPS_OPTIONS,
-    strictPort: true,
-    cors: CORS_OPTION
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
-});
+})
